c1cf728cc05e0c2d13441d5ad58b037a08fd807e
Raphaël Thériault <raphael_theriault@outlook.com>
2020-02-03 14:12:54 -0500

    void return type

--- src/cs.rs
+++ src/cs.rs
@@ -1,3 +1,7 @@
+use std::convert::Infallible;
+use std::str::FromStr;
+
+#[derive(Copy, Clone, Debug)]
 enum Type {
     I8,
     U8,
@@ -13,6 +17,7 @@ enum Type {
     Usize,
     String,
     Object,
+    Unit,
     Unknown,
 }
 
@@ -33,7 +38,39 @@ impl From<&str> for Type {
             "UintPtr" | "System.UIntPtr" => Self::Usize,
             "string" | "String" | "System.String" => Self::String,
             "object" | "Object" | "System.Object" => Self::Object,
+            "void" | "Void" | "System.Void" => Self::Unit,
             _ => Self::Unknown,
+        }
+    }
+}
+
+impl FromStr for Type {
+    type Err = Infallible;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(s.into())
+    }
+}
+
+impl Into<&'static str> for Type {
+    fn into(self) -> &'static str {
+        match self {
+            Self::I8 => "i8",
+            Self::U8 => "u8",
+            Self::I16 => "i16",
+            Self::U16 => "u16",
+            Self::I32 => "i32",
+            Self::U32 => "u32",
+            Self::I64 => "i64",
+            Self::U64 => "u64",
+            Self::F32 => "f32",
+            Self::F64 => "f64",
+            Self::ISize => "isize",
+            Self::Usize => "usize",
+            Self::String => "*mut symphony_il2cpp::types::Il2CppString",
+            Self::Object => "*mut symphony_il2cpp::types::Il2CppObject",
+            Self::Unit => "()",
+            Self::Unknown => "*mut std::ffi::c_void",
         }
     }
 }
